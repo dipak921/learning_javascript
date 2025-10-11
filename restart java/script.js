03ca58855847a38e8c69298e5679122fc15476c9
dipak921 <dipakchande921@gmail.com>
2025-10-12 00:20:11 +0530

    learn loop and string

--- restart java/script.js
+++ restart java/script.js
@@ -28,22 +28,83 @@
 
 // write a code which can give grades to students according to their scrores
 
-let score = prompt("Enter mark out of 100 ");
-if(score >=90 && score <=100){
-    console.log("A");
-} else if(score >=70 && score<=89){
-    console.log("B");
-}
- else if(score >=60 && score<=69){
-    console.log("C");
-}
- else if(score >=50 && score<=59){
-    console.log("D");
-} 
-else if (score >0 && score <= 49)
-{
-    console.log("F");
-}
-else{
-    console.log("Invalid");
-}
+// let score = prompt("Enter mark out of 100 ");
+// if(score >=90 && score <=100){
+//     console.log("A");
+// } else if(score >=70 && score<=89){
+//     console.log("B");
+// }
+//  else if(score >=60 && score<=69){
+//     console.log("C");
+// }
+//  else if(score >=50 && score<=59){
+//     console.log("D");
+// } 
+// else if (score >0 && score <= 49)
+// {
+//     console.log("F");
+// }
+// else{
+//     console.log("Invalid");
+// }
+
+//                  11-10-2025
+
+console.log("11/10/2025");
+
+// for(let i = 1; i<=7; i++){
+//     console.log("Dipak Chande ");
+// }
+
+//Calculate sumo of 1 to n 
+// let sum = 0;
+
+//     for(let i = 1; i<=10; i++){
+//         sum = sum + i;
+//     }
+//         console.log("total sum of n Number= ",sum);
+
+// print all even numbers from 0 to 100
+
+// for(let i = 0; i<=100; i++){
+//    if(i%2==0){ //even number
+//     console.log( i);
+//    }
+// }
+
+// create a game wher you start with any random game unmber ask the the user to keep guessing the game number until the user enters correct value
+
+// let gameNum = 25;
+// let userNum = prompt("Guess the game number :");
+// // console.log(userNum);
+// while(userNum != gameNum){
+//     userNum =  prompt("You enterd a wrong number  :");
+// }
+// console.log("Congratulations , you entered the right number");
+
+console.log("STRING");
+//string
+
+// let str = "dipak";
+// let str1 = 'chande';
+
+// console.log("Template litrels");
+// console.log("dipak \n  chande");
+
+console.log("STRING METHOD");
+// let str = "dipak chande";
+
+let str = 'dipak';
+let str1 = 'chande';
+// console.log(str.slice(1, 3));
+let res = str1.concat(str); // res = "My name is the :-" str + str1;
+console.log(res);
+
+ 
+
+
+
+
+
+
+
